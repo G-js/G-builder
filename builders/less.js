10c7898ea770ae43dab2a2f59c629cea17c452f6
j1anb1n <chan.jianbin@gmail.com>
2014-02-12 18:35:08 +0800

    update less builder

--- builders/less.js
+++ builders/less.js
@@ -2,6 +2,31 @@ var Parser = require('less').Parser;
 var _      = require('underscore');
 var path   = require('path');
 
+var URL_RE = /url\(('|")?(.*?)\1\)/g;
+
+function getChildResources (content, src, id) {
+    var deps = [];
+    var match;
+    var url;
+    URL_RE.lastIndex = 0;
+
+    while((match = URL_RE.exec(content))) {
+        url = match[2];
+        if (
+            url[0] !== '/' &&
+            url.indexOf('http') !== 0 &&
+            url.replace(/ /g, '') !== 'about:blank'
+        ) {
+            url = path.resolve(src, path.dirname(id), url).replace(src, '');
+            if (deps.indexOf(url) === -1) {
+                deps.push(url);
+            }
+        }
+    }
+
+    return deps;
+}
+
 module.exports = function (callback) {
     var fileInfo = this.file;
     var src = this.config.src;
@@ -30,11 +55,14 @@ module.exports = function (callback) {
         fileInfo.deps = fileInfo.deps.concat(imports);
 
         try {
-            fileInfo.output[fileInfo.id.replace(/\.less$/, '.css')] = tree.toCSS();
+            fileInfo.content = tree.toCSS();
+            fileInfo.output[fileInfo.id.replace(/\.less$/, '.css')] = fileInfo.content;
         } catch (ex) {
             fileInfo.warns.push(ex);
-            return callback(null);
+            fileInfo.content = '';
         }
+
+        fileInfo.deps = fileInfo.deps.concat(getChildResources(fileInfo.content, src, fileInfo.id));
 
         callback(null);
     });
