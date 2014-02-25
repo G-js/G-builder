cf906e50697e67b62947b5fb160c5b0083a1a673
j1anb1n <chan.jianbin@gmail.com>
2014-02-25 16:45:05 +0800

    add concat builder

---
+++ builders/concat.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var async = require('async');
+
+module.exports = function (callback) {
+    var fileInfo = this.file;
+    var src = this.config.src;
+    var children = fileInfo.content.split('\n');
+    fileInfo.deps = fileInfo.deps.concat(children);
+    async.map(
+        children,
+        function (file, next) {
+            fs.readFile(src + file, next);
+        },
+        function (err, buffers) {
+            if (err) {
+                return callback(err);
+            }
+            fileInfo.output[fileInfo.id] = buffers.map(function (buf) {
+                return buf.toString();
+            }).join('\n');
+
+            callback(null);
+        }
+    );
+};
