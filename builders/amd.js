e5a1e07e8aa98f2bc4a98c7288093ff75bad3926
j1anb1n <chan.jianbin@gmail.com>
2014-03-06 15:48:10 +0800

    update and deps parse

--- builders/amd.js
+++ builders/amd.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
-
-var REQUIRE_RE = /[^.]*\s*require\s*\(\s*(["'])([^'"\s\)]+)\1\s*\)/g;
+var REQUIRE_RE = /"(?:\\"|[^"])*"|'(?:\\'|[^'])*'|\/\*[\S\s]*?\*\/|\/(?:\\\/|[^\/\r\n])+\/(?=[^\/])|\/\/.*|\.\s*require|(?:^|[^$])\brequire\s*\(\s*(["'])(.+?)\1\s*\)/g;
+var SLASH_RE = /\\\\/g;
+var _ = require('underscore');
 
 function AMDBuilder (callback) {
     var fileInfo = this.file;
@@ -26,16 +27,21 @@ AMDBuilder.combine = function (callback) {
     callback(null);
 };
 
+function parseDependencies(code) {
+    var ret = [];
+
+    code.replace(SLASH_RE, "")
+        .replace(REQUIRE_RE, function(m, m1, m2) {
+            if (m2) {
+                ret.push(m2);
+            }
+        });
+
+    return _.unique(ret.sort(), true);
+}
+
 function transport (filename, content) {
-    var match = [];
-    var deps = [];
-    REQUIRE_RE.lastIndex = 0;
-
-    while((match = REQUIRE_RE.exec(content))) {
-        deps.push(match[2]);
-    }
-
-    deps = JSON.stringify(deps);
+    var deps = JSON.stringify(parseDependencies(content));
     return 'define("' + filename + '", ' + deps + ', function (require, exports, module) {\n' + content + '\n})';
 }
 
