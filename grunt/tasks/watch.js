59a7bf5114e520e387f886d1ddd2fe59eae246e4
zhangbingbing <zhangbingbing@ganji.com>
2014-05-04 15:53:48 +0800

    fixed win32 watch error

--- grunt/tasks/watch.js
+++ grunt/tasks/watch.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var spawn = require('child_process').spawn;
 var Gaze = require('gaze').Gaze;
+var platform = process.platform;
 
 module.exports = function(grunt) {
     grunt.registerTask('watch', function() {
@@ -13,28 +14,41 @@ module.exports = function(grunt) {
         grunt.log.subhead('Watching...');
 
         gaze.on('all', function (event, filepath) {
-            filepath = filepath.replace(path.resolve(src) + '/', '');
+			var pathSplit = '/';
+			if(platform === 'win32') {
+				pathSplit = '\\';
+			}
+            filepath = filepath.replace(path.resolve(src) + pathSplit, '');
             grunt.log.writeln('%s: %s', event.toUpperCase(), filepath);
-
+			
             if (event !== 'deleted') {
                 build(filepath);
             }
         });
 
         function build(file) {
-            var child = spawn('grunt', ['build:' + file]);
+			var cmd = 'grunt';
+			
+			if(platform === 'win32') {
+				file = file.replace(/\\/g, '/');
+				cmd = 'grunt.cmd';
+			}
+			
+            var child = spawn(cmd, ['build:'+file]);
             var output = '', errorMsg = '';
-
+			
             child.stdout.on('data', function (data) {
                 output += data;
             });
 
             child.stderr.on('data', function (data) {
+				console.log(1, data);
                 errorMsg += data;
             });
 
             child.on('exit', function (code) {
                 if (code !== 0) {
+					console.log("error");
                     grunt.log.error(errorMsg || output);
                     return;
                 }
